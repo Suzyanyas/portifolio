5b5859553ba8b936d8f1841e33810d58f31ee0d8
Suzyanyas <suzyalvessoares@gmail.com>
2024-06-27 12:26:04 +0100

    Descrição clara das alterações feitas

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,7 @@ document.querySelector(".menu-btn").addEventListener("click", () => {
     document.querySelector("ul.menu").classList.toggle("show");
   });
   
-  const whatsappNumber = "+5588992702014";
+  const whatsappNumber = "+351911919904";
   
   document.getElementById('contactForm').addEventListener('submit', function(event) {
     event.preventDefault(); // Previne o envio do formulário
